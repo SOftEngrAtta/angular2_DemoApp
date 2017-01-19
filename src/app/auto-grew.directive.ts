c90589fd05aa6d11a6074cfe3d6a0b99bd22f978
shamroze raza <shamroze.raza@gmail.com>
2017-01-19 12:56:12 +0500

    update code data

--- src/app/auto-grew.directive.ts
+++ src/app/auto-grew.directive.ts
@@ -3,19 +3,26 @@ import { Directive , ElementRef ,Renderer} from '@angular/core';
 @Directive({
   selector: '[appAutoGrew]',
   host : {
-    '(focus)' : 'onFocus()',
-    '(blur)' : 'onBlur()'
+    '(click)' : 'onFocus()'
   }
 })
 export class AutoGrewDirective {
-
+  private count : number = 0;
   constructor(private el : ElementRef , private render : Renderer){
-
+  
   }
   onFocus(){
-    this.render.setElementStyle(this.el.nativeElement,'width','400px');
+    if(this.count == 0){
+      this.render.setElementStyle(this.el.nativeElement,'width','400px');    
+    }
+    if(this.count == 1){
+      this.render.setElementStyle(this.el.nativeElement ,'width','120px');    
+    } 
+    this.count = this.count+1;
+    if(this.count > 1){
+      this.count = 0;
+    }
   }
-  onBlur(){
-    this.render.setElementStyle(this.el.nativeElement ,'width','120px');
-  }
+  // onBlur(){
+  // }
 }
